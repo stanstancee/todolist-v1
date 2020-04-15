8284f520fe4d765b5bb2a40a488741d8fe2b70f7
stanstancee <stanstancee@gmail.com>
2020-04-15 17:38:06 +0100

    Display date in my template current day

--- app.js
+++ app.js
@@ -7,16 +7,46 @@ let date = new Date();
 let weekDay = date.getDay();
 let day = "";
 
-app.get("/",function(req,res){
-    if(weekDay == 6 || weekDay == 0){
-        day = "weekend";
+app.get("/", function (req, res) {
+
+    switch (weekDay) {
+        case 0:
+            day = "Sunday";
+
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+
+            break;
+        case 3:
+            day = "Wednesday";
+
+            break;
+        case 4:
+            day = "Thursday";
+
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+
+
+        default:
+            console.log("Error please fix your code. Current day counts from 0-6. Current day is " + weekDay);
+            break;
+
     }
-    else{
-        day = "weekday"; 
-    }
-    res.render("list",{thisday:day});
+    res.render("list", {
+        thisday: day
+    });
 });
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log(`Server running on port ${PORT}`)
 });
