d3abd3bdd0705364b8a041d761f0ef09679ff9fc
stanstancee <stanstancee@gmail.com>
2020-04-16 14:29:32 +0100

    display date and add css

--- app.js
+++ app.js
@@ -1,51 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require('./date.js');
 const PORT = 3000;
 const app = express();
 app.set("view engine", "ejs");
-let date = new Date();
-let weekDay = date.getDay();
-let day = "";
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
 
 app.get("/", function (req, res) {
-
-    switch (weekDay) {
-        case 0:
-            day = "Sunday";
-
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-
-            break;
-        case 3:
-            day = "Wednesday";
-
-            break;
-        case 4:
-            day = "Thursday";
-
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-
-
-        default:
-            console.log("Error please fix your code. Current day counts from 0-6. Current day is " + weekDay);
-            break;
-
-    }
-    res.render("list", {
-        thisday: day
-    });
-});
+   const day = date.getDate();
+   res.render("list",{listTitle:day}) ;
+  
+ });
+  
 
 app.listen(PORT, function () {
     console.log(`Server running on port ${PORT}`)
